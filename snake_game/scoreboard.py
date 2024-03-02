b36de12b6e5c5e53f27bb72d0a107e2448e8de9b
JJimenez <j.jimenez.994@hotmail.com>
2024-03-02 15:14:36 -0500

    Starting to work with external files

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -7,18 +7,23 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt", mode="r") as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
         self.hideturtle()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=STYLE)
+        self.write(f"Score: {self.score} : High Score {self.high_score}", align=ALIGNMENT, font=STYLE)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"Game Over", align=ALIGNMENT, font=STYLE)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(f"{self.high_score}")
+        self.score = 0
+        self.update_score()
 
     def update_score(self):
         self.score += 1
         self.clear()
-        # self.goto(0, 270)
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=STYLE)
+        self.write(f"Score: {self.score} : High Score {self.high_score}", align=ALIGNMENT, font=STYLE)
--- snake_game/snake.py
+++ snake_game/snake.py
@@ -14,6 +14,13 @@ class Snake:
         self.create_snake()
         self.head = self.segments[0]
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def create_snake(self):
         for position in STARTING_POSITIONS:
             self.add_segment(position)
